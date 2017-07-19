b38727fd2e9747793f8781d8bf1bf65c168d7f86
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2017-07-18 22:19:40 -0400

    Summary: Shows QuakeCities as triangles in CityMap Week: 4 Programming Task: Custom Markers Programming SubTask: draw() method in the CityMarker class
    File:   src/module4/CityMarker.java
      Modifies: draw() adding colored triangles to represent cities.

--- src/module4/CityMarker.java
+++ src/module4/CityMarker.java
@@ -16,7 +16,7 @@ public class CityMarker extends SimplePointMarker {
 	
 	// The size of the triangle marker
 	// It's a good idea to use this variable in your draw method
-	public static final int TRI_SIZE = 5;  
+	public static final int TRI_SIZE = 3;  
 	
 	public CityMarker(Location location) {
 		super(location);
@@ -41,9 +41,9 @@ public class CityMarker extends SimplePointMarker {
 	public void draw(PGraphics pg, float x, float y) {
 		// Save previous drawing style
 		pg.pushStyle();
-		
-		// TODO: Add code to draw a triangle to represent the CityMarker
-		
+		pg.fill(255,200,60);
+		pg.stroke(255,200,60);
+		pg.triangle( x, (y - TRI_SIZE), (x + TRI_SIZE), (y + TRI_SIZE), (x - TRI_SIZE), (y + TRI_SIZE));
 		// Restore previous drawing style
 		pg.popStyle();
 	}
