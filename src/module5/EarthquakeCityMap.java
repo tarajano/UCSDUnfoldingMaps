115ca52ce26d2510556b9a9df3e7f193011bda01
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2017-07-21 03:55:18 -0400

    Summary: Adds Programming Task PDF file. Fixes paths to local data files. Week: 5 Programming Task: Responding to User Actions Programming SubTask: NA
    File: EarthquakeCityMap.java
      Modifies: Fix path to local data files.
    
    File: 04_Module.pdf
      Adds:  04_Module.pdf file

--- src/module5/EarthquakeCityMap.java
+++ src/module5/EarthquakeCityMap.java
@@ -41,11 +41,11 @@ public class EarthquakeCityMap extends PApplet {
 	public static String mbTilesString = "blankLight-1-3.mbtiles";
 	
 	//feed with magnitude 2.5+ Earthquakes
-	private String earthquakesURL = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.atom";
+	private String earthquakesURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.atom";
 	
 	// The files containing city names and info and country names and info
-	private String cityFile = "city-data.json";
-	private String countryFile = "countries.geo.json";
+	private String cityFile = "../data/city-data.json";
+	private String countryFile = "../data/countries.geo.json";
 	
 	// The map
 	private UnfoldingMap map;
@@ -72,7 +72,7 @@ public class EarthquakeCityMap extends PApplet {
 		else {
 			map = new UnfoldingMap(this, 200, 50, 650, 600, new Google.GoogleMapProvider());
 			// IF YOU WANT TO TEST WITH A LOCAL FILE, uncomment the next line
-		    //earthquakesURL = "2.5_week.atom";
+		    earthquakesURL = "../data/2.5_week.atom";
 		}
 		MapUtils.createDefaultEventDispatcher(this, map);
 		
