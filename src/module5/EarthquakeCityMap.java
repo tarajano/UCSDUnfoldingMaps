4f9228efc837c89e5aa81e6eef49f9269b86f346
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2017-07-22 22:19:17 -0400

    Summary: Adds selectMarkerIfHover() in EarthquakeCityMap Week: 5 Programming Task: Responding to User Actions Programming SubTask: Implement selectMarkerIfHover in EarthquakeCityMap
    File: EarthquakeCityMap
      Adds: selectMarkerIfHover() to select a mapmarker if mouse hovers.
      Modifies: swap googlemapprovider by microsoftmapprovider
    
    File: CityMarker
      Modifies: makes it a child of (extends) CommonMarker instead of SimplePointMarker

--- src/module5/EarthquakeCityMap.java
+++ src/module5/EarthquakeCityMap.java
@@ -11,7 +11,7 @@ import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.marker.AbstractShapeMarker;
 import de.fhpotsdam.unfolding.marker.Marker;
 import de.fhpotsdam.unfolding.marker.MultiMarker;
-import de.fhpotsdam.unfolding.providers.Google;
+import de.fhpotsdam.unfolding.providers.Microsoft;
 import de.fhpotsdam.unfolding.providers.MBTilesMapProvider;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 import parsing.ParseFeed;
@@ -70,7 +70,7 @@ public class EarthquakeCityMap extends PApplet {
 		    earthquakesURL = "2.5_week.atom";  // The same feed, but saved August 7, 2015
 		}
 		else {
-			map = new UnfoldingMap(this, 200, 50, 650, 600, new Google.GoogleMapProvider());
+			map = new UnfoldingMap(this, 200, 50, 650, 600, new Microsoft.RoadProvider());
 			// IF YOU WANT TO TEST WITH A LOCAL FILE, uncomment the next line
 		    earthquakesURL = "../data/2.5_week.atom";
 		}
@@ -146,6 +146,15 @@ public class EarthquakeCityMap extends PApplet {
 	private void selectMarkerIfHover(List<Marker> markers)
 	{
 		// TODO: Implement this method
+		for(Marker m: markers){
+			if( m.isInside( map, mouseX, mouseY) ){
+				System.out.println(m.getProperties().toString());
+				lastSelected = (CommonMarker) m;
+				lastSelected.setSelected(true);
+				m.setSelected(true);
+				return;
+			}
+		}
 	}
 	
 	/** The event handler for mouse clicks
