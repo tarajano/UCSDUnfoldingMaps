ad44a30a2219a8bc2045bc53525100fe434c2386
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2017-08-30 13:47:26 -0400

    Summary: Adds helper methods to link/filter airport and routes data. Week: 6 Programming Task: Programming SubTask:
    File:   src/module6/AirportMarker.java
      Adds: setRoutesTraffic() computes the traffic (number of flights) on a route and set it as a property of SimpleLinesMarker (route) objects.
      Adds: setRoutesMidPoint() computes the coordinates of the route mid point and set it as a property of SimpleLinesMarker (route) objects.
      Adds: setRoutesDistance() and getRouteDistance() compute the length (Km) of a route and set it as a property of SimpleLinesMarker (route) objects
      Adds: deleteDuplicateRoutes() once the route traffic propery had been set, this method removes redundant source/destination airports pairs
            which happens due to 1) inbound/outbound flights and 2) multiple airlines covering the same route. (Only one marker is needed to draw the route).
      Adds: assignRoutesToAirports() links routes to airports using the routes field in AirportMarker objects.
      Adds: printAirportMarkers() printRouteProperties() to print data to stdout.
      Removes: checkLocationsPresenceInRoutesList()
      Renames: getRoutesConnectingLastClicked() --> getRoutesConnectingAirport()
      Renames: unSelectMarkers() --> deselectMarkers()

--- src/module6/AirportMap.java
+++ src/module6/AirportMap.java
@@ -1,9 +1,11 @@
 package module6;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 import org.apache.commons.collections4.ListUtils;
@@ -34,7 +36,7 @@ public class AirportMap extends PApplet {
 	private List<Marker> airportList;
 	List<Marker> routeList;
 	
-	private CommonMarker lastSelected;
+//	private CommonMarker lastSelected;
 	private CommonMarker lastClicked;
 	
 	public void setup() {
@@ -83,12 +85,18 @@ public class AirportMap extends PApplet {
 				route.addLocation(locSource);
 				route.addLocation(locDestination);
 				SimpleLinesMarker sl = new SimpleLinesMarker(route.getLocations(), route.getProperties());
+				sl.setProperty("midPointCoords", getRouteMidPoint(sl));
 				routeList.add(sl);
-				
 			}
 		}
-		
-		routeList = setRouteTraffic(routeList);
+		// Linking data
+		routeList = setRoutesTraffic(routeList);
+		//printRouteProperties();
+		routeList = deleteDuplicateRoutes(routeList);
+		routeList = setRoutesDistance(routeList);
+		routeList = setRoutesMidPoint(routeList);
+		assignRoutesToAirports();
+//		printAirportMarkers();
 		map.addMarkers(routeList);
 		map.addMarkers(airportList);
 	}
@@ -108,21 +116,118 @@ public class AirportMap extends PApplet {
 		
 		if (lastClicked == null) {
 			displayMarkers(airportList);
-			unSelectMarkers(airportList);
+			deselectMarkers(airportList);
 			displayMarkers(routeList); 
 		} else {
-			// hide all markers
+			// first hide all markers
 			hideMarkers(airportList);
 			hideMarkers(routeList);
-			// display markers related to lastClicked
+			// then display markers related to lastClicked (airports and routes)
 			lastClicked.setHidden(false);
 			displayMarkers(getAirportsConnectedToLastClicked(lastClicked));
-			displayMarkers(getRoutesConnectingLastClicked(lastClicked));
+			displayMarkers(getRoutesConnectingAirport(lastClicked));
 		}
 	}
 	
 	// Helper methods
-	private List<Marker> setRouteTraffic(List<Marker> rList) {
+	private Location getRouteMidPoint(SimpleLinesMarker slm) {
+		Location midPoint = null;
+		List<Location> locs = slm.getLocations();
+		float x1 = locs.get(0).getLat();
+		float y1 = locs.get(0).getLon();
+		float x2 = locs.get(1).getLat();
+		float y2 = locs.get(1).getLon();
+		float midX = (x1 + x2)/2;
+		float midY = (y1 + y2)/2;
+		midPoint = new Location(midX,midY);
+		return midPoint;
+	}
+	
+	private void printAirportMarkers() {
+		for (Marker m : airportList ) {
+			AirportMarker am = (AirportMarker) m;
+			System.out.println( m.getProperties().toString() + " routes: " + am.getRoutes().size() ) ; 
+		}
+	}
+	
+	private void printRouteProperties() {
+		for (Marker m : routeList) {
+			SimpleLinesMarker slm = (SimpleLinesMarker) m;
+			System.out.println( " route -- slm: " + slm.getProperties().toString()) ; 
+		}
+	}
+
+	private List<Marker> deleteDuplicateRoutes(List<Marker> rList) {
+		// This method should be executed ONLY after the route traffic 
+		// has been computed. This method will remove route 'duplicates'.
+		// Duplicates are defined here as repeated source/destination 
+		// airport pairs, which happens due to 1) inbound/outbound flights
+		// and 2) multiple airlines covering the same route. 
+		List<Marker> returnRouteList = new ArrayList<Marker>();
+		List<List<Location>> locsCheckList = new ArrayList<List<Location>>();
+		
+//		System.out.println("deleteDuplicateRoutes input List size: " + rList.size());
+		
+		for (Marker m : rList) {
+			SimpleLinesMarker slm = (SimpleLinesMarker) m;
+			List<Location> locs = slm.getLocations();
+			List<Location> locsSwaped = Arrays.asList(locs.get(1), locs.get(0));
+			
+			// Avoid duplicate source/destination | destination/source pairs
+			if (! locsCheckList.contains(locs) & ! locsCheckList.contains(locsSwaped)) {
+				locsCheckList.add(locs);
+				returnRouteList.add(slm);
+			}
+			
+//			System.out.println("   route: " + slm.getProperties().toString() );
+		}
+		
+//		System.out.println("deleteDuplicateRoutes return List size: " + returnRouteList.size());
+		
+		return returnRouteList;
+	}
+	
+	private List<Marker> setRoutesMidPoint(List<Marker> rList) {
+		List<Marker> returnRouteList = new ArrayList<Marker>();
+		
+		for (Marker m : rList) {
+			SimpleLinesMarker slm = (SimpleLinesMarker) m;
+			slm.setProperty("midPointCoords", getRouteMidPoint(slm) );
+			returnRouteList.add(slm);
+		}
+		
+		return returnRouteList;
+	}
+	
+	private List<Marker> setRoutesDistance(List<Marker> rList) {
+		List<Marker> returnRouteList = new ArrayList<Marker>();
+		
+		for (Marker m : rList) {
+			SimpleLinesMarker slm = (SimpleLinesMarker) m;
+			slm.setProperty("routeDistanceKm", getRouteDistance(slm) );
+			returnRouteList.add(slm);
+		}
+		
+		return returnRouteList;
+	}
+	
+	private Double getRouteDistance(SimpleLinesMarker slm) {
+		Double routeDistance = null;
+		DecimalFormat df = new DecimalFormat("#.0");
+		List<Location> locs = slm.getLocations();
+		routeDistance = (Double) locs.get(0).getDistance(locs.get(1));
+		routeDistance = Double.parseDouble( df.format(routeDistance) );
+		return routeDistance;
+	}
+	
+	private void assignRoutesToAirports() {
+		for (Marker am : airportList) {
+			AirportMarker airport = (AirportMarker) am;
+			airport.setRoutes( (List<SimpleLinesMarker>)(List<?>) getRoutesConnectingAirport(airport) );
+		}
+	}
+	
+	private List<Marker> setRoutesTraffic(List<Marker> rList) {
 		HashMap<List<Location>,Integer> routesTrafficHashMap = new HashMap<List<Location>,Integer>(); 
 		List<Marker> returnRouteList = new ArrayList<Marker>();
 		Integer traffic = 1;
@@ -167,37 +272,26 @@ public class AirportMap extends PApplet {
 	private int getColor(int traffic) {
 		int col = 0;
 		colorMode(RGB, 100);
-		int alpha = 100 - (100 - traffic);
-		col = color(200,0,0,alpha);
+		int alpha = (int) (100 - (100 - Math.pow(traffic, 1.5)));
+		col = color(255,0,0,alpha);
 		return col;
 	}
-	
-	private boolean checkLocationsPresenceInRoutesList(Location source, Location destination) {
-		for (Marker m: routeList) {
-			SimpleLinesMarker slm = (SimpleLinesMarker) m;
-			List<Location> locs = slm.getLocations();
-			if ( locs.contains(source) & locs.contains(destination) ) {
-				return true;
-			}
-		}
-		return false;
-	}
 
-	private List<Marker> getRoutesConnectingLastClicked( CommonMarker last ) {
-		Marker lastClicked = (Marker) last;
+	private List<Marker> getRoutesConnectingAirport( CommonMarker cmarker ) {
+		Marker airport = (Marker) cmarker;
 		List<Marker> connectingRoutes = new ArrayList<Marker>();
-		Integer lastClickedId = Integer.parseInt((String)lastClicked.getId());
+		Integer airportId = Integer.parseInt((String)airport.getId());
 		
 		for(Marker route : routeList) {
 			SimpleLinesMarker slmRoute = (SimpleLinesMarker) route;
 			Integer sourceId =  Integer.parseInt((String)slmRoute.getProperty("source"));
 			Integer destId =  Integer.parseInt((String)slmRoute.getProperty("destination"));
 			
-			if ( lastClickedId.equals(sourceId) | lastClickedId.equals(destId)) {
+			if ( airportId.equals(sourceId) | airportId.equals(destId)) {
 				connectingRoutes.add(route);
 			}			
 		}
-		
+		System.out.println("airportId, connectingRoutes: " + airportId + " " + connectingRoutes.size());
 		return connectingRoutes;
 	}
 	
@@ -226,11 +320,12 @@ public class AirportMap extends PApplet {
 	private List<Marker> getAirportsById(List<Integer> idList) {
 		List<Marker> airportsReturn = new ArrayList<Marker>();
 		// TODO make arg idList unique
+		idList = new ArrayList<>(new HashSet<>(idList));
 		for (Integer refereceId : idList) {
 			for (Marker airport : airportList) {
 				Integer targetId =  Integer.parseInt((String)airport.getId());
 				if ( targetId.equals(refereceId) ) {
-					System.out.println(" connection: " + airport.getProperties().toString());
+//					System.out.println(" connection: " + airport.getProperties().toString());
 					airportsReturn.add(airport);
 				}
 			}
@@ -259,7 +354,7 @@ public class AirportMap extends PApplet {
 		} 
 	}
 	
-	private void unSelectMarkers(List<Marker> markers) {
+	private void deselectMarkers(List<Marker> markers) {
 		for(Marker marker : markers) {
 			marker.setSelected(false); 
 		}
